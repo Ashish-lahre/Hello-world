8d449d50d2ec6b4ce4ebc58632ad266af67eedb1
Ashish-lahre <69587090+Ashish-lahre@users.noreply.github.com>
2020-08-12 21:16:08 -0700

    path changer
    this can change path of any file

---
+++ path changer.py
@@ -0,0 +1,14 @@
+import pathlib
+import shutil
+import time
+from_path = pathlib.Path('C:/users/Durga/Downloads')
+while True:
+    for file in from_path.iterdir():
+        file_in_string = str(file)
+        if '.jpg' in file_in_string:
+            time.sleep(0.005)
+            shutil.move(str(file), "E:\jpg")
+
+
+
+
